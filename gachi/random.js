ae29f5647cbb6e784d758aa39d951d2abe961e4b
Yaroslav1734 <86847741+Yaroslav1734@users.noreply.github.com>
2022-06-02 16:53:29 +0300

    Add files via upload

---
+++ gachi/random.js
@@ -0,0 +1,14 @@
+function randpng()
+{
+    var pngs = [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg',
+        '7.jpg'
+    ];
+    return pngs[Math.floor(Math.random() * pngs.length)];
+}
+document.write('hikka-gallery.tk/gachi/' + randpng())
