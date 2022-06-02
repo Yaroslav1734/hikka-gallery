2c1d3c06ab0705fcc12690acaf9f693b1a7aa9b8
Yaroslav1734 <86847741+Yaroslav1734@users.noreply.github.com>
2022-06-02 16:50:18 +0300

    Add files via upload

---
+++ milos/random.js
@@ -0,0 +1,22 @@
+function randpng()
+{
+    var pngs = [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg',
+        '7.jpg',
+        '8.jpg',
+        '9.jpg',
+        '10.jpg',
+        '11.jpg',
+        '12.jpg',
+        '13.jpg',
+        '14.jpg',
+        '15.jpg'
+    ];
+    return jpgs[Math.floor(Math.random() * pngs.length)];
+}
+document.write('hikka-gallery.tk/milos/' + randpng())
